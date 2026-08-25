11b192ada81b5785e0d47d43b942cb800e328f50
Super User <root@192.168.1.76>
2026-08-25 15:02:47 +0530

    minor env bug resolve

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ load_dotenv()  # Load environment variables from .env file
 
 app = Quart(__name__)
 app = cors(app, allow_credentials=True,
-    allow_origin=os.getenv(DASHBOARD_DOMAIN),
+    allow_origin=os.getenv("DASHBOARD_DOMAIN"),
     # send_origin_wildcard=False,
     max_age=timedelta(days=1))
 
