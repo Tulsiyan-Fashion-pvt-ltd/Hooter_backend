b74df47a63d9563580677d4c2f6acd1f36f89e22
Farhan <farhanhmadpy@gmail.com>
2026-09-23 18:11:27 +0530

    privacy update

--- pages.py
+++ pages.py
@@ -5,3 +5,7 @@ page = Blueprint('page', __name__,template_folder="templates")
 @page.route('/')
 async def index():
     return await render_template('index.html')
+
+@page.route('/privacy')
+async def privacy_policy():
+    return await render_template('privacy-policy.html')
